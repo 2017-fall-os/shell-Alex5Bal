00d77e7d6b64bc379c8ae818af77846f18eeda42
Alex5Bal <abalderrama5@outlook.com>
2017-10-08 19:40:15 -0600

    Add files via upload

--- shell/mystrlib.c
+++ shell/mystrlib.c
@@ -1,95 +1,103 @@
+/*
+ * Author: Alejandro Balderrama
+ * Class: Theory of Operating Systems
+ * Professor: Dr. Freudenthal
+ * Project: Shell
+ *
+ * Description: The following program implements a string tokenizer. This implementation works by taking a string, partitioning
+ * 				its individual tokens by the ' ' character, then storing each token into a vector.
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include "mystrlib.h"
 
-int strComp(char* string1, char* string2)
+int strComp(char* string1, char* string2) /**** Function compares two strings and returns 1 if it's a match ****/
 {
-	char match = 1;
+	char match = 1;		/** Initially match condition is true **/
 	int i = 0;
 
-	while(string1[i] != '\0')
+	while(string1[i] != '\0')		/** Traverse string until null character is found **/
 	{
-		if(string1[i] != string2[i])
-			match = 0;
+		if(string1[i] != string2[i])	/** If the characters a the current index don't match; **/
+			match = 0;		/** Set match to false **/
 
 		i++;
 	}
 
-	if(string2[i] != '\0')
+	if(string2[i] != '\0')		/** Check to see if one of the strings is longer than the other **/
 		match = 0;
 
 	return match;
 }
 
-int strLen(char* stringIn)
+int strLen(char* stringIn) /**** Function returns the length of a string ****/
 {
-	int length = 0, i = 0;
+	int length = 0, i = 0;		/** Initialize length to 0 **/
 
-	while(stringIn[i] != '\0')
+	while(stringIn[i] != '\0')		/** Traverse string until null character is found **/
 	{
-		length++;
+		length++;		/** Increment the length by 1 every iteration **/
 		i++;
 	}
 	return length;
 }
 
-char* strCat(char* string1, char* string2)
+char* strCat(char* string1, char* string2) /**** Function concatenates two strings and returns a pointer to a string ****/
 {
-	char* stringOut;
+	char* stringOut;		/** String pointer to be returned **/
 	int length1, length2, i, j;
 
-	length1 = strLen(string1);
-	length2 = strLen(string2);
+	length1 = strLen(string1);		/** Determine the length of the first string **/
+	length2 = strLen(string2);		/** Determine the length of the second string **/
 
-	stringOut = (char*) malloc(length1 + length2 + 1);
+	stringOut = (char*) malloc(length1 + length2 + 1);		/** Use the lengths of the two strings to allocate memory for the new string **/
 
-	for(i = 0; i < length1; i++)
+	for(i = 0; i < length1; i++)		/** Transfer first string into the new string **/
 	{
 		stringOut[i] = string1[i];
 	}
 
-	for(j = 0; j < length2; j++)
+	for(j = 0; j < length2; j++)		/** Append the second string into the new string **/
 	{
 		stringOut[i + j] = string2[j];
 	}
 
-	stringOut[i + j] = '\0';
+	stringOut[i + j] = '\0';		/** End the new string with a null character **/
 
 	return stringOut;
 }
 
-char* strCopy(char* dest, char* src)
+char* strCopy(char* dest, char* src) /**** Function copies one string to another ****/
 {
 	int i = 0;
-	//int srcLength = strLen(src);
 
-	while(src[i] != '\0')
+	while(src[i] != '\0')	/** Traverse the source string until null character is found **/
 	{
-		dest[i] = src[i];
+		dest[i] = src[i];	/** Copy each character from the source string to the destination string **/
 		i++;
 	}
 
-	dest[i] = '\0';
+	dest[i] = '\0';		/** End destination string with the null character **/
 
 	return dest;
 }
 
-void printStringVec(char** vectorIn)
+void printStringVec(char** vectorIn)	/**** Function prints out a vector of strings ****/
 {
-	for(int i = 0; vectorIn[i] != '\0'; i++)
+	for(int i = 0; vectorIn[i] != '\0'; i++)		/** Traverse vector until null character is found **/
 	{
-		printf("path[%d] : %s\n", i, vectorIn[i]);
+		printf("path[%d] : %s\n", i, vectorIn[i]);		/** Print out the string at each vector position **/
 	}
 }
 
-char* removeNewLine(char* stringIn)
+char* removeNewLine(char* stringIn)		/**** Function removes the newline character from the end of a string ****/
 {
-	char* stringOut;
+	char* stringOut;		/** String pointer to be returned **/
 
-	for(int i = 0; stringIn[i] != '\0'; i++)
+	for(int i = 0; stringIn[i] != '\0'; i++)		/** Traverse string until null character is found **/
 	{
-		if(stringIn[i] == '\n')
-			stringIn[i] = '\0';
+		if(stringIn[i] == '\n')			/** If newline character is found; **/
+			stringIn[i] = '\0';			/** Replace with null character **/
 	}
 
 	stringOut = stringIn;
--- shell/shell.c
+++ shell/shell.c
@@ -1,3 +1,12 @@
+/*
+ * Author: Alejandro Balderrama
+ * Class: Theory of Operating Systems
+ * Professor: Dr. Freudenthal
+ * Project: Shell
+ *
+ * Description: The following program implements a string tokenizer. This implementation works by taking a string, partitioning
+ * 				its individual tokens by the ' ' character, then storing each token into a vector.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,73 +18,73 @@
 
 int main(int argc, char** argv, char** envp)
 {
-	char** args;	//Stores the arguments passed by the user
-	char** tempVec;	//Temporary vector used to hold the value of each key in the envp[]
-	char** path;	//Stores the value of PATH
-	char* string;	//Stores the buffer input
-	char* command;	//Stores the command given by the user
-	int pid;        //Stores value of child process after fork()
+	char** args;	/** Stores the arguments passed by the user **/
+	char** tempVec;	/** Temporary vector used to hold the value of each key in the envp[] **/
+	char** path;	/** Stores the value of PATH **/
+	char* string;	/** Stores the buffer input **/
+	char* command;	/** Stores the command given by the user **/
+	int pid;		/** Stores value of child process after fork() **/
 
 	while(1)
 	{
 		string = (char*) malloc(BUFFERSIZE);
 
 		write(1, "$ ", 2);
-		read(0, string, BUFFERSIZE);	//Read buffer input into 'string'
+		read(0, string, BUFFERSIZE);	/** Read buffer input into 'string' **/
 
-		removeNewLine(string);    //Remove '\n' character from the end of the input string
+		removeNewLine(string);	/** Remove the '\n' character from the end of the input string **/
 
 		if(string[0] == 'X')
 			exit(0);
 
 		else
 		{
-			args = Mytoc(string, ' ');	//Separate individual arguments by a ' '
+			args = Mytoc(string, ' ');	/** Separate individual arguments by a ' ' **/
 
-			pid = fork();	//Create child process
+			pid = fork();	/** Create child process **/
 
-			if(pid < 0)		//Child process creation was unsuccessful
+			if(pid < 0)		/** Child process creation was unsuccessful **/
 			{
 				perror("Fork failed\n");
 				exit(1);
 			}
-			else if(pid == 0)	//Child process created successfully
+			else if(pid == 0)	/** Child process created successfully **/
 			{
-				for(int i = 0; envp[i] != '\0'; i++)	//Traverse envp[]
+				for(int i = 0; envp[i] != '\0'; i++)	/** Traverse envp[] **/
 				{
-					tempVec = Mytoc(envp[i], '=');	//Identify key and value by tokenizing with '='
+					tempVec = Mytoc(envp[i], '=');	/** Identify key and value by tokenizing with '=' **/
 
-					if(strComp(tempVec[0], "PATH"))	//If the key value is PATH
+					if(strComp(tempVec[0], "PATH"))		/** If the key value is PATH **/
 					{
-						path = Mytoc(tempVec[1], ':');	//Tokenize the value of PATH with ':'
-						break;	//Exit loop, path has been found
+						path = Mytoc(tempVec[1], ':');	/** Tokenize the value of PATH with ':' **/
+						break;	/** Exit loop, path has been found **/
 					}
 
-					free(tempVec);	//Ready for next iteration
+					free(tempVec);	/** Ready for next iteration **/
 				}
 
-				command = strCat("/", args[0]);	//Ready user command to be used w/ the path
+				command = strCat("/", args[0]);	/** Ready user command to be used w/ the path **/
 
-				for(int j = 0; path[j] != '\0'; j++)	//Traverse the path vector until valid path is executed
+				for(int j = 0; path[j] != '\0'; j++)	/** Traverse the path vector until valid path is executed **/
 				{
-					free(args[0]);	//Ready args[0] to store the complete path
-					args[0] = strCat(path[j], command);	//args[0] contains complete path
+					free(args[0]);	/** Ready args[0] to store the complete path **/
+					args[0] = strCat(path[j], command);	/** args[0] contains complete path **/
 					printf("%s\n", args[0]);
-					execve(args[0], args, envp);	//Execute the complete path if valid
+					execve(args[0], args, envp);	/** Execute the complete path if valid **/
 				}
 
-				printf("Command not found\n");	//No valid commands were identified
+				printf("Command not found\n");	/** No valid commands were identified **/
 				exit(0);
 			}
 
 			else
 			{
-				wait(NULL);	//Wait for the child process to end
+				wait(pid);	/** Wait for the child process to end **/
 			}
 		}
-		free(string);	//Ready string for next buffer input
+		free(string);	/** Ready string for next buffer input **/
 
-		for (int i = 0; args[i] != '\0'; i++)	//Ready args[] for next command
+		for (int i = 0; args[i] != '\0'; i++)	/** Ready args[] for next command **/
 		{
 			free(args[i]);
 		}
